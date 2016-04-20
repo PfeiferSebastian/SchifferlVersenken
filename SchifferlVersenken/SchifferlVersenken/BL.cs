1248f19b12510a2e114a7967a063ee95839af3bd
Sebastian Pfeifer <nagrond@hotmail.com>
2016-04-20 16:56:53 +0200

    Basisstruktur

---
+++ SchifferlVersenken/SchifferlVersenken/BL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchifferlVersenken
+{
+    internal class BL
+    {
+        #region Felder & Eigenschaften
+        //Spieler
+        public string P1Name { get; set; }
+        public string P2Name { get; set; }
+        public int AktuellerSpieler { get; set; }
+
+        //Schiffe
+        public int Traeger { get; set; }
+        public int Schlachtschiff { get; set; }
+        public int Kreuzer { get; set; }
+        public int Zerstoerer { get; set; }
+        #endregion
+
+        #region Methoden
+
+        //Für Methoden im Spiel
+
+        public void platzierung_p1()
+        { }
+
+        public void platzierung_p2()
+        { }
+
+        public void positionscheck()
+        { }
+
+        public void spielzug(int spieler)
+        { }
+
+        public void siegesbedingung()
+        { }
+
+        #endregion
+
+
+
+
+    }
+}
---
+++ SchifferlVersenken/SchifferlVersenken/Spielfeld.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchifferlVersenken
+{
+    internal static class Spielfeld
+    {
+        //Arrays für die Spieler
+
+        //Spieler 1
+        
+        static string [,,] P1Array = new string[10, 10, 2];
+
+        //Spieler 2
+
+        static string[,,] P2Array = new string[10, 10, 2];
+
+    }
+}
