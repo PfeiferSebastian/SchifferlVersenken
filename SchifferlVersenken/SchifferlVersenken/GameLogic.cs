5dc6d4aa7d9d48a4d468abce7799fb6c1f60ab06
Emre Yüksel <yueksel_emre@live.at>
2016-04-21 13:21:03 +0200

    Klasse für Schiffe angelegt

---
+++ SchifferlVersenken/SchifferlVersenken/GameLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchifferlVersenken
+{
+    public class GameLogic
+    {
+
+        #region Methoden
+
+        //Für Methoden im Spiel
+
+        public void platzierung_p1()
+        { }
+
+        public void platzierung_p2()
+        { }
+
+        public void positionscheck()
+        { }
+
+        public void spielzug(int spieler)
+        { }
+
+        public void siegesbedingung()
+        { }
+
+        #endregion
+
+    }
+}
---
+++ SchifferlVersenken/SchifferlVersenken/Spieler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchifferlVersenken
+{
+    internal class Spieler
+    {
+        #region Felder & Eigenschaften
+        //Spieler
+        public string P1Name { get; set; }
+        public string P2Name { get; set; }
+        public int AktuellerSpieler { get; set; }
+
+        //Schiffe (Wird zurzeit nicht benötigt)
+        //public int Traeger { get; set; }
+        //public int Schlachtschiff { get; set; }
+        //public int Kreuzer { get; set; }
+        //public int Zerstoerer { get; set; }
+        #endregion
+
+        
+
+
+
+
+    }
+}
